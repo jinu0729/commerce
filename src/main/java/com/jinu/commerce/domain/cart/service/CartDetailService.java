c268c2a442094f421ef8f209981e386e0a74ff50
jinu0729 <jinu0729@gmail.com>
2024-04-27 00:27:23 +0900

    [Feat] 장바구니 아이템 수량 변경 구현 #32

--- src/main/java/com/jinu/commerce/domain/cart/service/CartDetailService.java
+++ src/main/java/com/jinu/commerce/domain/cart/service/CartDetailService.java
@@ -1,6 +1,7 @@
 package com.jinu.commerce.domain.cart.service;
 
 import com.jinu.commerce.domain.cart.dto.request.CartRequestDto;
+import com.jinu.commerce.domain.cart.dto.request.CartItemEditRequestDto;
 import com.jinu.commerce.domain.cart.entity.Cart;
 import com.jinu.commerce.domain.cart.entity.CartItem;
 
@@ -10,4 +11,8 @@ public interface CartDetailService {
     void addCartItems(Cart cart, List<CartRequestDto> requestDtos);
 
     List<CartItem> getAllCartItemsByCart(Cart cart);
+
+    void editCartItemQty(CartItemEditRequestDto cartItemId);
+
+    CartItem getCartItemById(Long cartItemId);
 }
